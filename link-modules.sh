a590d6a9f87e0d8aee358db9f0e8071588c4416c
dalmamad <dalmohamad97@gmail.com>
2023-12-15 03:55:33 +0330

    changing symbolic link script

--- link-modules.sh
+++ link-modules.sh
@@ -1,32 +1,23 @@
-# Add your links source and destination_directory here
-declare -A i3wm_module_list=(
-[0,0]="i3" [0,1]="$HOME/.config"
-[1,0]="picom" [1,1]="$HOME/.config"
-[2,0]="rofi" [2,1]="$HOME/.config"
-[3,0]="dunst" [3,1]="$HOME/.config"
-[4,0]=".xprofile" [4,1]="$HOME"
-)
+get_link_name(){
+  local link_location=$1
 
-declare -A nvim_module_list=(
-[0,0]="nvim" [0,1]="$HOME/.config"
-)
+  IFS="/" read -ra parts <<< "$link_location"
+  local link_name="${parts[-1]}"
 
-declare -A tools_module_list=(
-[0,0]="alacritty.yml" [0,1]="$HOME/.config"
-[1,0]="lazygit" [1,1]="$HOME/.config"
-[2,0]=".bashrc" [2,1]="$HOME"
-)
+  echo $link_name
+}
 
+link_modules(){
+  local link_source=$1
+  local destination_dir=$2
+  local link_name=$(get_link_name $link_source)
 
-link_each(){
-  link_source=$1
-  destination_dir=$2
 
   # Check if the symbolic link already exists
-  if [ -L "$destination_dir/$link_source" ]; then
+  if [ -L "$destination_dir/$link_name" ]; then
     # Remove the existing symbolic link
-    rm -rf "$destination_dir/$link_source"
-    echo "Existing symbolic link for $link_source removed."
+    rm -rf "$destination_dir/$link_name"
+    echo "Existing symbolic link for $link_name removed."
   fi
 
 # Create the new symbolic link
@@ -34,21 +25,18 @@ ln -s "$(pwd)/$link_source" "$destination_dir/"
 echo "Symbolic link for $link_source created."
 }
 
-link_source_list() {
-  local -n source_list=$1
-  length=$((${#source_list[@]} / 2))
+# Creating symbolic link
+## i3wm
+link_modules "i3wm/i3" "$HOME/.config"
+link_modules "i3wm/picom" "$HOME/.config"
+link_modules "i3wm/rofi" "$HOME/.config"
+link_modules "i3wm/dunst" "$HOME/.config"
+link_modules "i3wm/xprofile/.xprofile" "$HOME"
 
-  for ((i=0; i<$length; i++)); do
-    link_each ${source_list[$i,0]} ${source_list[$i,1]}
-  done
-}
+## nvim
+link_modules "nvim" "$HOME/.config"
 
-cd ./i3wm
-link_source_list i3wm_module_list
-cd ../
-
-link_source_list nvim_module_list
-
-cd ./tools
-link_source_list tools_module_list
-cd ../
+## tools
+link_modules "tools/alacritty/alacritty.yml" "$HOME/.config"
+link_modules "tools/lazygit" "$HOME/.config"
+link_modules "tools/bashrc/.bashrc" "$HOME"

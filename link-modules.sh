a3b45363b9311792cac875a75f467ec9dfc86154
mamad <dal.mohamad97@gmail.com>
2024-08-31 16:06:54 +0330

    removing old install script

--- link-modules.sh
+++ link-modules.sh
@@ -22,34 +22,8 @@ link_modules(){
   fi
 
 # Create the new symbolic link
-echo "$current_dir"
 sudo ln -s "$current_dir/$link_source" "$destination_dir/"
 echo "Symbolic link for $link_source created."
 }
 
-# Creating symbolic link
-## i3wm
-link_modules "i3wm/i3" "$HOME/.config"
-link_modules "i3wm/picom" "$HOME/.config"
-link_modules "i3wm/rofi" "$HOME/.config"
-link_modules "i3wm/dunst" "$HOME/.config"
-link_modules "i3wm/xprofile/.xprofile" "$HOME"
-
-## hyprland
-link_modules "hyprland/hypr" "$HOME/.config"
-link_modules "hyprland/waybar" "$HOME/.config"
-link_modules "hyprland/wofi" "$HOME/.config"
-link_modules "hyprland/mako" "$HOME/.config"
-
-## nvim
-link_modules "nvim" "$HOME/.config"
-
-## tools
-link_modules "alacritty/alacritty.toml" "$HOME/.config"
-link_modules "lazygit" "$HOME/.config"
-link_modules "foot" "$HOME/.config"
-link_modules "bashrc/.bashrc" "$HOME"
-link_modules "zsh/.zshrc" "$HOME"
-link_modules "tmux/.tmux.conf" "$HOME"
-link_modules "lf" "$HOME/.config"
-link_modules "keyd/default.conf" "/etc/keyd"
+link_modules $1 $2
